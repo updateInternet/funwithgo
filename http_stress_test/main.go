0240ef0d12f836a8e1a47f09b30a804aa333dd0c
Vasanth R S <vrs1886@gmail.com>
2016-10-03 19:45:30 +1100

    http stress test

---
+++ http_stress_test/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	tn := time.Now()
+	out := make(chan string)
+	for i := 1; i <= 10; i++ {
+		go htmlTime(out, i)
+		//fmt.Print(<-out) I could print this here... but
+	}
+	// if you don't make two separate loops you'll never get the go routines
+	// running concurrently as <- out will block
+	for i := 1; i <= 10; i++ {
+		fmt.Print(<-out)
+	}
+	dur := time.Since(tn)
+	fmt.Printf("Done in %v\n", dur)
+}
+
+func htmlTime(out chan<- string, i int) {
+	tn := time.Now()
+	//res, err := http.Get("http://www.gutenberg.org/cache/epub/1661/pg1661.txt")
+	// when testing this.. Gutenberg.org blocked my ip  :D
+	res, err := http.Get("http://www.apple.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	dur := time.Since(tn)
+	out <- fmt.Sprintf("%v, URL Download Time: %v, Loop Number: %v\n", res.Status, dur, i)
+	res.Body.Close()
+}
